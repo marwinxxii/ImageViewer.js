cd9a19a82bd67528a030e12f65a0b750df78db52
Alexey Agapitov <marwinxxii@gmail.com>
2013-07-04 02:13:46 +0400

    Load each image when it is requested for view.

--- main.js
+++ main.js
@@ -4,9 +4,9 @@ var ImageViewer;
     'use strict';
 
     function fullscreenSupported() {
-        return document.requestFullscreen ||
-            document.mozRequestFullScreen ||
-            document.webkitRequestFullscreen;
+        return document.documentElement.requestFullscreen ||
+            document.documentElement.mozRequestFullScreen ||
+            document.documentElement.webkitRequestFullscreen;
     }
 
     function requestFullscreen(elem) {
@@ -27,77 +27,112 @@ var ImageViewer;
     */
     ImageViewer = function(args) {
         this._element = document.getElementById(args.imageholder);
-        this._size = 0;
-        this._index = 0;
+        this._setStartValues();
+
+        this._reader = new FileReader();
+        this._reader.onload = this._onImageRead.bind(this);
 
         document.getElementById(args.files).addEventListener('change',
             this._onFilesSelected.bind(this), false);
+
         if (args.fullscreener && fullscreenSupported()) {
             document.getElementById(args.fullscreener).addEventListener(
-                'click', this.startFullscreen, false);
+                'click', this.startFullscreen.bind(this), false);
         }
+
         document.addEventListener('keypress', this._onKeyPress.bind(this),
             false);
+
+        this._element.addEventListener('click',
+            this._onShowNext.bind(this), false);
     };
 
     var SPACE = 32, ARROW_RIGHT = 39, ARROW_LEFT = 37;
 
     ImageViewer.prototype = {
+        showImage: function(index) {
+            if (index < 0 || index > this._files.length) return;
+            if (index in this._images) {
+                this._showImage(index);
+            } else {
+                this._loadImage(index);
+            }
+        },
+
         startFullscreen: function() {
             requestFullscreen(this._element);
         },
 
-        _onKeyPress: function(e) {
-            if (e.keyCode === SPACE || e.keyCode === ARROW_RIGHT) {
-                this.showNextImage();
-            }
-            else if (e.keyCode === ARROW_LEFT) {
-                this.showPreviousImage();
-            }
-        },
-
-        showNextImage: function() {
-            var imgs = this._element.getElementsByTagName('img');
-            imgs[this._index].className = 'hidden';
-            this._index = (this._index + 1) % this._size;
-            imgs[this._index].className = '';
-        },
-
-        showPreviousImage: function() {
-            var imgs = this._element.getElementsByTagName('img');
-            imgs[this._index].className = 'hidden';
-            if (this._index === 0)
-                this._index = this._size - 1;
-            else
-                this._index -= 1;
-            imgs[this._index].className = '';
+        _setStartValues: function() {
+            this._index = 0;
+            this._files = [];
+            this._images = {};
+            this._loadingId = null;
         },
 
         _onFilesSelected: function(evt) {
             var files = evt.target.files;
             if (!files || files.length === 0) return;
+            this._setStartValues();
             this._element.innerHTML = '';
-            var reader, self = this;
-            for (var i = 0; i < files.length; i++)
+            var i, file;
+            for (i = 0; i < files.length; i++)
             {
-                reader = new FileReader();
-                reader.onload = (function(index){
-                    return function(e)
-                    {
-                        var img = document.createElement('img');
-                        if (index !== 0) {
-                            img.className = 'hidden';
-                        }
-                        img.src = e.target.result;
-                        self._element.appendChild(img);
-                    };
-                })(i);
-                reader.readAsDataURL(files[i]);
+                file = files[i];
+                this._files.push(file);
             }
-            this._size = files.length;
-            this._element.addEventListener('click',
-                this.showNextImage.bind(this), false);
-            this._index = 0;
+            this.showImage(0);
+        },
+
+        _onImageRead: function(e) {
+            var img = document.createElement('img');
+            img.src = e.target.result;
+            img.className = 'hidden';
+            this._element.appendChild(img);
+            this._images[this._loadingId] = img;
+            this._showImage(this._loadingId);
+            this._loadingId = null;
+        },
+
+        _onKeyPress: function(e) {
+            if (this._files.length < 1) return;
+            if (e.keyCode === SPACE || e.keyCode === ARROW_RIGHT) {
+                this._onShowNext();
+            }
+            else if (e.keyCode === ARROW_LEFT) {
+                this._onShowPrevious();
+            }
+        },
+
+        _onMouseClick: function() {
+            if (this._files.length < 1) return;
+            this._onShowNext();
+        },
+
+        _onShowNext: function() {
+            this.showImage((this._index + 1) % this._files.length);
+        },
+
+        _onShowPrevious: function() {
+            var index;
+            if (this._index === 0)
+                index = this._files.length - 1;
+            else
+                index = this._index - 1;
+            this.showImage(index);
+        },
+
+        _loadImage: function(index) {
+            if (this._reader.readyState !== FileReader.LOADING) {
+                this._loadingId = index;
+                this._reader.readAsDataURL(this._files[index]);
+            }
+        },
+
+        _showImage: function(index) {
+            this._images[this._index].className = 'hidden';
+            this._index = index;
+            this._images[this._index].className = '';
         }
     };
 })();
