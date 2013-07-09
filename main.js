0e334792cb26fa0b2a1461390a8ca1e55217ea47
Alexey Agapitov <marwinxxii@gmail.com>
2013-07-09 21:39:19 +0400

    Plugin API and new plugin for showing image number and total count. Clear images using node deletion, to prevent removing other elements inside viewer container.

--- main.js
+++ main.js
@@ -19,6 +19,17 @@ var ImageViewer;
         }
     }
 
+    var plugins = {
+        index: function(viewer) {
+            var element = document.getElementById('index');
+            viewer.addEventListener('imageShown', function(e) {
+                element.style.display = 'block';
+                element.innerHTML = (e.detail.index + 1) + '/' +
+                    e.detail.viewer.count;
+            });
+        }
+    };
+
     /*args: {
         imageholder: "id of element where images will be placed",
         fullscreener: "id of clickable element activating fullscreen mode",
@@ -28,6 +39,10 @@ var ImageViewer;
     ImageViewer = function(args) {
         this._element = document.getElementById(args.imageholder);
         this._setStartValues();
+
+        Object.defineProperty(this, "count", {
+            get: function() { return this._files.length; }
+        });
 
         this._reader = new FileReader();
         this._reader.onload = this._onImageRead.bind(this);
@@ -45,6 +60,16 @@ var ImageViewer;
 
         this._element.addEventListener('click',
             this._onShowNext.bind(this), false);
+
+        if (args.plugins) {
+            var p, i;
+            for (i = 0; i < args.plugins.length; i++) {
+                p = args.plugins[i];
+                if (p in plugins) {
+                    plugins[p](this);
+                }
+            }
+        }
     };
 
     var SPACE = 32, ARROW_RIGHT = 39, ARROW_LEFT = 37;
@@ -63,6 +88,10 @@ var ImageViewer;
             requestFullscreen(this._element);
         },
 
+        addEventListener: function(event, callback) {
+            document.addEventListener(event, callback);
+        },
+
         _setStartValues: function() {
             this._index = 0;
             this._files = [];
@@ -74,13 +103,25 @@ var ImageViewer;
             var files = evt.target.files;
             if (!files || files.length === 0) return;
             this._setStartValues();
-            this._element.innerHTML = '';
-            var i, file;
+            var i = 0,
+                child,
+                childrenCount = this._element.children.length;
+            while (i < this._element.children.length) {
+                child = this._element.children[i];
+                if (child.nodeName === 'IMG') {
+                    this._element.removeChild(child);
+                } else {
+                    i++;
+                }
+            }
+            var file;
             for (i = 0; i < files.length; i++)
             {
                 file = files[i];
                 this._files.push(file);
             }
+
+            this._dispatchEvent('filesSelected');
             this.showImage(0);
         },
 
@@ -133,6 +174,23 @@ var ImageViewer;
             this._images[this._index].className = 'hidden';
             this._index = index;
             this._images[this._index].className = '';
+            this._dispatchEvent('imageShown', {
+                index: index,
+                file: this._files[index]
+            });
+        },
+
+        _dispatchEvent: function(name, detail) {
+            if (detail === undefined) {
+                detail = {};
+            }
+            detail.viewer = this;
+            var evt = new CustomEvent(name, {
+                bubbles: false,
+                cancelable: false,
+                detail: detail
+            });
+            document.dispatchEvent(evt);
         }
     };
 })();
